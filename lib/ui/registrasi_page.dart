520796924ec7fbc640941eb7411586592e032f67
Abidluayraihantaufik <spaskeren30@gmail.com>
2024-05-07 10:32:43 +0700

    feat: add folder image

--- lib/ui/registrasi_page.dart
+++ lib/ui/registrasi_page.dart
@@ -68,7 +68,7 @@ class _RegistrasiPageState extends State<RegistrasiPage>{
 
         Pattern pattern = r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0 -9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$';
 
-        
+
         return null;
       },
     );
